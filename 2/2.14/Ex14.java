eb6f838b4991ffaf93f39117888f8490bb035016
Aris <aris8konstantinou@gmail.com>
2017-07-05 22:33:10 +0300

    Exercise14

---
+++ 2/2.14/Ex14.java
@@ -0,0 +1,23 @@
+
+public class Ex14 {
+	
+	public static void main(String[] args){
+	/*using pirntln we have to print everything in
+	 * a command because println prints a new line
+	 */
+	
+	System.out.println("1 2 3 4");
+	
+	/*using print we can print each number in a different command
+	 * but there is no reason to do so in this particular occassion
+	 */
+	
+	System.out.print("1 2 3 4");
+	
+	/*using printf we will use escape characters which they are
+	 * very useful when you need to print data on screen
+	 */
+	
+	System.out.printf("\n%s", "1 2 3 4");
+	}
+}
