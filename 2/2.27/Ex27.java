8603ad962289e14d0d9c263eb097c2c50a9a831d
Aris <aris8konstantinou@gmail.com>
2017-07-11 11:13:07 +0300

    Add files via upload

---
+++ 2/2.27/Ex27.java
@@ -0,0 +1,13 @@
+
+public class Ex27 {
+	public static void main(String[] args){
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+	}
+}
