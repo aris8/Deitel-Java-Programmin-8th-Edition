aeacbcaa6defe01b63f45dcc106dded1e752032e
Aris <aris8konstantinou@gmail.com>
2017-07-11 11:52:54 +0300

    Add files via upload

---
+++ 2/2.31/Ex31.java
@@ -0,0 +1,17 @@
+
+public class Ex31 {
+	public static void main(String[] args){
+		System.out.printf("%s\t  %s\t%s\n", "αριθμός","τετράγωνο","κύβος");
+		System.out.printf("%d\t  %d\t\t%d\n",0,0,0);
+		System.out.printf("%d\t  %d\t\t%d\n",1,1,1);
+		System.out.printf("%d\t  %d\t\t%d\n",2,4,8);
+		System.out.printf("%d\t  %d\t\t%d\n",3,9,27);
+		System.out.printf("%d\t  %d\t\t%d\n",4,16,64);
+		System.out.printf("%d\t  %d\t\t%d\n",5,25,125);
+		System.out.printf("%d\t  %d\t\t%d\n",6,36,216);
+		System.out.printf("%d\t  %d\t\t%d\n",7,49,343);
+		System.out.printf("%d\t  %d\t\t%d\n",8,64,512);
+		System.out.printf("%d\t  %d\t\t%d\n",9,81,729);
+		System.out.printf("%d\t  %d\t\t%d\n",10,100,1000);
+	}
+} 
